ec951481575d2818505c67db1586f64fe025aa8b
Andrew Weiss <andreivayss@gmail.com>
2022-04-27 14:08:46 +0700

    Added validation of required token type

--- src/main/java/parser/Parser.kt
+++ src/main/java/parser/Parser.kt
@@ -1,4 +1,34 @@
 package parser
 
-class Parser  {
+import lexer.Token
+import lexer.TokenType
+
+class Parser(private val tokens: List<Token>)  {
+    var pos: Int = 0
+    val scope: HashMap<String, Any> = hashMapOf()
+
+
+    /**
+     * Используется если мы в конце ожидаем; или если у нас есть '(' значит мы ожидаем ')'
+     */
+    private fun require(tokenType: TokenType): Token {
+        val token = match(tokenType)
+        token?.let {
+            return it
+        } ?: throw Exception("on position $pos expected ${tokenType.label}")
+    }
+
+    /**
+     * По текущей позиции возвращает токен из списка
+     */
+    private fun match(tokenType: TokenType): Token? {
+        if (pos < tokens.size) {
+            val currentToken = tokens[pos]
+            if (tokenType == currentToken.type) {
+                pos += 1
+                return currentToken
+            }
+        }
+        return null
+    }
 }
